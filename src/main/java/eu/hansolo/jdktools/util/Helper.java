46ebef6ef1dc3534817f5aef6694a11ab10c7f19
Gerrit Grunwald <han.solo@mac.com>
2022-02-24 11:46:45 +0100

    Latest additions from jdk17 branch

--- src/main/java/eu/hansolo/jdktools/util/Helper.java
+++ src/main/java/eu/hansolo/jdktools/util/Helper.java
@@ -18,12 +18,34 @@
 
 package eu.hansolo.jdktools.util;
 
+import eu.hansolo.jdktools.Architecture;
 import eu.hansolo.jdktools.Constants;
+import eu.hansolo.jdktools.OperatingMode;
+import eu.hansolo.jdktools.OperatingSystem;
+import eu.hansolo.jdktools.OsArcMode;
 import eu.hansolo.jdktools.TermOfSupport;
 import eu.hansolo.jdktools.versioning.VersionNumber;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 
 public class Helper {
+
+    private static final String[] DETECT_ALPINE_CMDS       = { "/bin/sh", "-c", "cat /etc/os-release | grep 'NAME=' | grep -ic 'Alpine'" };
+    private static final String[] UX_DETECT_ARCH_CMDS      = { "/bin/sh", "-c", "uname -m" };
+    private static final String[] MAC_DETECT_ROSETTA2_CMDS = { "/bin/sh", "-c", "sysctl -in sysctl.proc_translated" };
+    private static final String[] WIN_DETECT_ARCH_CMDS     = { "cmd.exe", "/c", "SET Processor" };
+    private static final Pattern  ARCHITECTURE_PATTERN     = Pattern.compile("(PROCESSOR_ARCHITECTURE)=([a-zA-Z0-9_\\-]+)");
+    private static final Matcher  ARCHITECTURE_MATCHER     = ARCHITECTURE_PATTERN.matcher("");
+
 
     private Helper(){}
 
@@ -90,6 +112,86 @@ public class Helper {
             return TermOfSupport.STS;
         } else {
             return TermOfSupport.NOT_FOUND;
+        }
+    }
+
+    public static final OperatingSystem getOperatingSystem() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.indexOf("win") >= 0) {
+            return OperatingSystem.WINDOWS;
+        } else if (os.indexOf("apple") >= 0 || os.indexOf("mac") >= 0) {
+            return OperatingSystem.MACOS;
+        } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
+            try {
+                final ProcessBuilder processBuilder = new ProcessBuilder(DETECT_ALPINE_CMDS);
+                final Process        process        = processBuilder.start();
+                final String         result         = new BufferedReader(new InputStreamReader(process.getInputStream())).lines().collect(Collectors.joining("\n"));
+                return null == result ? OperatingSystem.LINUX : result.equals("1") ? OperatingSystem.ALPINE_LINUX : OperatingSystem.LINUX;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return OperatingSystem.LINUX;
+            }
+        } else if (os.indexOf("sunos") >= 0) {
+            return OperatingSystem.SOLARIS;
+        } else {
+            return OperatingSystem.NOT_FOUND;
+        }
+    }
+
+    public static final Architecture getArchitecture() {
+        final String arch = System.getProperty("os.arch").toLowerCase(Locale.ENGLISH);
+        if (arch.contains("sparc")) return Architecture.SPARC;
+        if (arch.contains("amd64") || arch.contains("86_64")) return Architecture.AMD64;
+        if (arch.contains("86")) return Architecture.X86;
+        if (arch.contains("s390x")) return Architecture.S390X;
+        if (arch.contains("ppc64")) return Architecture.PPC64;
+        if (arch.contains("arm") && arch.contains("64")) return Architecture.AARCH64;
+        if (arch.contains("arm")) return Architecture.ARM;
+        if (arch.contains("aarch64")) return Architecture.AARCH64;
+        return Architecture.NOT_FOUND;
+    }
+
+    public static final OsArcMode getOperaringSystemArchitectureOperatingMode() {
+        final OperatingSystem operatingSystem = getOperatingSystem();
+        try {
+            final ProcessBuilder processBuilder = OperatingSystem.WINDOWS == operatingSystem ? new ProcessBuilder(WIN_DETECT_ARCH_CMDS) : new ProcessBuilder(UX_DETECT_ARCH_CMDS);
+            final Process        process        = processBuilder.start();
+            final String         result         = new BufferedReader(new InputStreamReader(process.getInputStream())).lines().collect(Collectors.joining("\n"));
+            switch(operatingSystem) {
+                case WINDOWS:
+                    ARCHITECTURE_MATCHER.reset(result);
+                    final List<MatchResult> results     = ARCHITECTURE_MATCHER.results().collect(Collectors.toList());
+                    final int               noOfResults = results.size();
+                    if (noOfResults > 0) {
+                        final MatchResult   res = results.get(0);
+                        return new OsArcMode(operatingSystem, Architecture.fromText(res.group(2)), OperatingMode.NATIVE);
+                    } else {
+                        return new OsArcMode(operatingSystem, Architecture.NOT_FOUND, OperatingMode.NOT_FOUND);
+                    }
+                case MACOS:
+                    Architecture architecture = Architecture.fromText(result);
+                    final ProcessBuilder processBuilder1 = new ProcessBuilder(MAC_DETECT_ROSETTA2_CMDS);
+                    final Process        process1        = processBuilder1.start();
+                    final String         result1         = new BufferedReader(new InputStreamReader(process1.getInputStream())).lines().collect(Collectors.joining("\n"));
+                    return new OsArcMode(operatingSystem, architecture, result1.equals("1") ? OperatingMode.EMULATED : OperatingMode.NATIVE);
+                case LINUX:
+                    return new OsArcMode(operatingSystem, Architecture.fromText(result), OperatingMode.NATIVE);
+            }
+
+            // If not found yet try via system property
+            final String arch = System.getProperty("os.arch").toLowerCase(Locale.ENGLISH);
+            if (arch.contains("sparc"))                           { return new OsArcMode(operatingSystem, Architecture.SPARC, OperatingMode.NATIVE); }
+            if (arch.contains("amd64") || arch.contains("86_64")) { return new OsArcMode(operatingSystem, Architecture.AMD64, OperatingMode.NATIVE); }
+            if (arch.contains("86"))                              { return new OsArcMode(operatingSystem, Architecture.X86, OperatingMode.NATIVE); }
+            if (arch.contains("s390x"))                           { return new OsArcMode(operatingSystem, Architecture.S390X, OperatingMode.NATIVE); }
+            if (arch.contains("ppc64"))                           { return new OsArcMode(operatingSystem, Architecture.PPC64, OperatingMode.NATIVE); }
+            if (arch.contains("arm") && arch.contains("64"))      { return new OsArcMode(operatingSystem, Architecture.AARCH64, OperatingMode.NATIVE); }
+            if (arch.contains("arm"))                             { return new OsArcMode(operatingSystem, Architecture.ARM, OperatingMode.NATIVE); }
+            if (arch.contains("aarch64"))                         { return new OsArcMode(operatingSystem, Architecture.AARCH64, OperatingMode.NATIVE); }
+            return new OsArcMode(operatingSystem, Architecture.NOT_FOUND, OperatingMode.NATIVE);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new OsArcMode(operatingSystem, Architecture.NOT_FOUND, OperatingMode.NATIVE);
         }
     }
 }
