5ebb2728f2ddf871ce77eefac9f8e60bafb34811
Gerrit Grunwald <han.solo@mac.com>
2024-07-15 09:16:29 +0200

    Minor fix for CVSS

--- src/main/java/eu/hansolo/jdktools/CVSS.java
+++ src/main/java/eu/hansolo/jdktools/CVSS.java
@@ -1,15 +1,20 @@
 package eu.hansolo.jdktools;
 
 public enum CVSS {
-    CVSSV2, CVSSV3, CVSSV4, NOT_FOUND;
+    CVSSV2,
+    CVSSV3,
+    CVSSV4,
+    NOT_FOUND;
+
+
 
     public static CVSS fromText(final String text) {
         if (null == text) { return NOT_FOUND; }
         switch (text) {
-            case "CVSSV2", "cvssV2", "cvssv2" -> { return CVSSV2; }
-            case "CVSSV3", "cvssV3", "cvssv3" -> { return CVSSV3; }
-            case "CVSSV4", "cvssV4", "cvssv4" -> { return CVSSV4; }
-            default                           -> { return NOT_FOUND; }
+            case "cvss_20", "CVSSV2", "cvssV2", "cvssv2" -> { return CVSSV2; }
+            case "cvss_30", "CVSSV3", "cvssV3", "cvssv3" -> { return CVSSV3; }
+            case "cvss_40", "CVSSV4", "cvssV4", "cvssv4" -> { return CVSSV4; }
+            default                                      -> { return NOT_FOUND; }
         }
     }
 }
